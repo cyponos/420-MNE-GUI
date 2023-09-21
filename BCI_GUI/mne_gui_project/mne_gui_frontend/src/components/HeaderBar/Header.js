ad1f5b4709bda2959e20421b9835bb952423e3b3
CB9611 <cameron.branch@selju.edu>
2023-09-21 11:43:23 -0500

    Small Frontend Changes, Added HeaderBar

---
+++ BCI_GUI/mne_gui_project/mne_gui_frontend/src/components/HeaderBar/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header-Container">
+      <div className="Header">
+        <div className="Title">
+          Header
+        </div>
+        <input className="Search-Bar" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
