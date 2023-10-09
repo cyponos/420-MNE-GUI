7766c6474cac784bee6b19ecbf9ff34e916e0bbb
Cameron <cameron.branch@selu.edu>
2023-10-09 12:12:12 -0500

    Frontend and Backend talk now!

--- BCI_GUI/mne_gui_project/mne_gui_app/urls.py
+++ BCI_GUI/mne_gui_project/mne_gui_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from mne_gui_app.views import *
 from . import views
 
 urlpatterns = [
+
+    path('', ReactView.as_view(), name="BCI_GUI"),
     path('upload_file', views.upload_file, name='upload_file'),
-    path('', views.home, name='home'),
     path('file_list/', views.file_list, name='file_list'),
     path('delete_file/<int:file_id>/', views.delete_file, name='delete_file'),
     path('view/<int:eeg_data_id>/', views.view_data, name='view_data'),
--- BCI_GUI/mne_gui_project/mne_gui_frontend/src/App.js
+++ BCI_GUI/mne_gui_project/mne_gui_frontend/src/App.js
@@ -1,32 +1,64 @@
+import React from 'react';
+import axios from 'axios';
 import Header from './components/HeaderBar/Header.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="app-container">
-        <Header />
-        <div className="body-content">
-          <div className="flex-column">
-            <div className="flex-selection">
-              File Selection
+class App extends React.Component {
+
+  state = { details: [], }
+
+  componentDidMount() {
+
+    let data;
+    axios.get('http://localhost:8000')
+    .then(res => {
+      data = res.data;
+      this.setState({
+        details: data
+      });
+    })
+    .catch(err => { })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="app-container">
+          <Header />
+          <div className="body-content">
+            <div className="flex-column">
+              <div className="flex-selection">
+                File Selection
+
+                {/* Temporary Test Data */}
+                {this.state.details.map((output, id) =>
+                  <div key={id}>
+                    <div>
+                      {output.name}
+                      {output.detail}
+                    </div>
+                  </div>
+                )}
+                {/* Temporary Test Data */}
+
+              </div>
+              <div className="flex-selection">
+                Filter Selection
+              </div>
             </div>
-            <div className="flex-selection">
-              Filter Selection
-            </div>
-          </div>
-          <div className="flex-column">
-            <div className="flex-selection">
-              Channel Selection
-            </div>
-            <div className="flex-selection">
-              Output Selection
+            <div className="flex-column">
+              <div className="flex-selection">
+                Channel Selection
+              </div>
+              <div className="flex-selection">
+                Output Selection
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
